22385680d32f6e1cc4e9fef2df1462bb63eec0b2
Derrick Harms <derrick.harms@cerner.com>
2014-06-02 16:04:44 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Derrick Harms"
+    email     "derrick.harms@gmail.com"
+    password  "foobar"
+    password_confirmation  "foobar"
+  end
+end
